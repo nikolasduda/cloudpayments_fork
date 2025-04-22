0eef171b377dd704ba31f10d8a30f052403f5fac
Nikolay Dudin <mle8@yandex.ru>
2025-04-22 16:43:05 +0300

    update kotlin plugin code

--- android/src/main/kotlin/com/shushper/cloudpayments/CloudpaymentsPlugin.kt
+++ android/src/main/kotlin/com/shushper/cloudpayments/CloudpaymentsPlugin.kt
@@ -20,7 +20,6 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.UnsupportedEncodingException
 import java.security.InvalidKeyException
 import java.security.NoSuchAlgorithmException
